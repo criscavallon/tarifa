4a27220505b199ec25f94cdcaee0996c78fd565e
Eduardo Santos <eduardo@yacows.com.br>
2019-11-06 18:48:21 -0300

    Alteração do componente inicial

--- src/App.js
+++ src/App.js
@@ -2,29 +2,60 @@ import React from 'react';
 import App from './App';
 
 class tarifa extends React.Component {
-  render() {
-    return (
-<div class="Screen MatcScreen">
-  <div class="Column_1">
-    <div class="Row_1"><img src="undefined/ddd.jpg" class="Image" />
-     <div class="Primary_Label">Como Funciona</div>
-     <div class="Label_1">São apenas 2 passos:
-     1. Escolha o serviço bancário que deseja comparar.&nbsp;
-     2. Informe a quantidade que utiliza por mês.
-    </div>
-   </div>
-   <div class="Row_2">
-    <div class="DropDown" />
-    <div class="DropDown_1" />
-    <div class="OK">OK</div>
-   </div>
-   <div class="Row_3">
-     <div class="Primary_Label_1">Quem Somos</div>
-     <div class="Label">O Preço da conta PJ é um comparativo de tarifas de vários bancos para ajudar o empresário de Micro e Pequenas Empresas na escolha do menor preço. </div>
-   </div>
-  </div>
-</div>
-);
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+    }
+
+
+    componentDidMount() {
+        fetch("https://olinda.bcb.gov.br/olinda/servico/Informes_ListaTarifaPorValores/versao/v1/odata/GruposConsolidados?%24format=json\" -H  \"accept: application/json;odata.metadata=minimal")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result.items
+                    });
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+
+    render() {
+        const { error, isLoaded, items } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+
+                <ul>
+                    {items.map(item => (
+                            <li key={item.name}>
+                                {item.name} {item.price}
+                            </li>
+                        ))}
+                </ul>
+
+            );
+
+        }
   }
 }
 
